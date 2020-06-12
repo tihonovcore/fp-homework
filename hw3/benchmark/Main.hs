08ddef944a437042df198d1a411f6d6164b302b1
Vitaly <tihonovcore@gmail.com>
2020-06-12 12:30:06 +0300

    [hw3/task 1] naive version & doc

--- hw3/benchmark/Main.hs
+++ hw3/benchmark/Main.hs
@@ -23,13 +23,23 @@ main = do
   !points6 <- randomNPoints 1000000
   !points7 <- randomNPoints 10000000
   defaultMain [
-    bgroup "accumPerimeter" [ bench "1e5" $ whnf perimeter points5
-                            , bench "1e6" $ whnf perimeter points6
-                            , bench "1e7" $ whnf perimeter points7
+    bgroup "accumPerimeter" [ bench "1e5" $ whnf perimeter points5        -- 2.49 ms
+                            , bench "1e6" $ whnf perimeter points6        -- 15.2 ms
+                            , bench "1e7" $ whnf perimeter points7        -- 99.8 ms
                             ]
     ,
-    bgroup "accumDoubleArea" [ bench "1e5" $ whnf doubleArea points5
-                             , bench "1e6" $ whnf doubleArea points6
-                             , bench "1e7" $ whnf doubleArea points7
+    bgroup "accumDoubleArea" [ bench "1e5" $ whnf doubleArea points5      -- 1.24 ms
+                             , bench "1e6" $ whnf doubleArea points6      -- 10.1 ms
+                             , bench "1e7" $ whnf doubleArea points7      -- 67.6 ms
+                             ]
+    ,
+    bgroup "naivePerimeter" [ bench "1e5" $ whnf naivePerimeter points5   -- 6.78 ms
+                            , bench "1e6" $ whnf naivePerimeter points6   -- 94   ms
+                            , bench "1e7" $ whnf naivePerimeter points7   -- 770  ms
+                            ]
+    ,
+    bgroup "naiveDoubleArea" [ bench "1e5" $ whnf naiveDoubleArea points5 -- 5.6  ms
+                             , bench "1e6" $ whnf naiveDoubleArea points6 -- 71.6 ms
+                             , bench "1e7" $ whnf naiveDoubleArea points7 -- 816  ms
                              ]
     ]
--- hw3/src/Task1.hs
+++ hw3/src/Task1.hs
@@ -3,42 +3,71 @@ module Task1 where
 
 --import Debug.Trace (traceShowId)
 
+-- | Representation of two-dimensional point
 data Point = Point
            { x :: !Int
            , y :: !Int
            }
   deriving (Show, Eq)
 
+-- | Sums two points by coordinates
 plus :: Point -> Point -> Point
 plus a b = Point (x a + x b) (y a + y b)
 
+-- | Subtract two points by coordinates
 minus :: Point -> Point -> Point
 minus a b = Point (x a - x b) (y a - y b)
 
+-- | Evaluate distance between points
 size :: Point -> Double
 size p = sqrt . fromIntegral $ (x p ^ 2) + (y p ^ 2)
 
+-- | Evaluate scalar product of two points
 scalarProduct :: Point -> Point -> Int
 scalarProduct a b = (x a * x b) + (y a * y b)
 
+-- | Evaluate cross product of two points
 crossProduct :: Point -> Point -> Int
 crossProduct a b = (x a * y b) - (x b * y a)
 
+-- | Evaluate perimeter of polygon without self-intersections strictly
 perimeter :: [Point] -> Double
 perimeter []           = 0
-perimeter (xx : other) = countPerimeter 0 xx xx other
+perimeter (first : list) = countPerimeter 0 first list
   where
-    countPerimeter :: Double -> Point -> Point -> [Point] -> Double
-    countPerimeter !accum first prev (p : other1) = countPerimeter (accum + size (minus prev p)) first p other1
-    countPerimeter !accum first prev []           = accum + size (minus first prev)
+    countPerimeter :: Double -> Point -> [Point] -> Double
+    countPerimeter !accum prev (p : other) = countPerimeter (accum + size (minus prev p)) p other
+    countPerimeter !accum prev []           = accum + size (minus first prev)
 
+-- | Evaluate doubled area of polygon without self-intersections strictly
 doubleArea :: [Point] -> Int
 doubleArea []          = 0
-doubleArea (xx : list) = countDoubleArea 0 xx xx list
+doubleArea (first : list) = evalDoubleArea 0 first list
   where
-    countDoubleArea :: Int -> Point -> Point -> [Point] -> Int
-    countDoubleArea !accum first prev (p : other) = countDoubleArea (accum + trapezoidDoubleArea prev p) first p other
-    countDoubleArea !accum first prev []          = accum + trapezoidDoubleArea prev first
+    evalDoubleArea :: Int -> Point -> [Point] -> Int
+    evalDoubleArea !accum prev (p : other) = evalDoubleArea (accum + trapezoidDoubleArea prev p) p other
+    evalDoubleArea !accum prev []          = accum + trapezoidDoubleArea prev first
+
+    trapezoidDoubleArea :: Point -> Point -> Int
+    trapezoidDoubleArea p1 p2 = (y p1 + y p2) * (x p2 - x p1)
+
+-- | Evaluate perimeter of polygon without self-intersections __not__ strictly
+naivePerimeter :: [Point] -> Double
+naivePerimeter []                         = 0
+naivePerimeter (firstPoint : otherPoints) = evalNaivePerimeter firstPoint otherPoints
+  where
+    evalNaivePerimeter :: Point -> [Point] -> Double
+    evalNaivePerimeter prev (p : other) = size (minus prev p) + evalNaivePerimeter p other
+    evalNaivePerimeter prev []          = size (minus firstPoint prev)
+
+-- | Evaluate doubled area of polygon without self-intersections __not__ strictly
+naiveDoubleArea :: [Point] -> Int
+naiveDoubleArea []          = 0
+naiveDoubleArea (first : list) = evalNaiveDoubleArea first list
+  where
+    evalNaiveDoubleArea :: Point -> [Point] -> Int
+    evalNaiveDoubleArea prev (p : other) = trapezoidDoubleArea prev p + evalNaiveDoubleArea p other
+    evalNaiveDoubleArea prev []          = trapezoidDoubleArea prev first
 
     trapezoidDoubleArea :: Point -> Point -> Int
     trapezoidDoubleArea p1 p2 = (y p1 + y p2) * (x p2 - x p1)
